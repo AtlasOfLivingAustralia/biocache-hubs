e2fd5aeea43eb4cb4282fc88fd44d1c68149ef3f
nickdos <nickdos@gmail.com>
2014-04-14 01:20:35 +0000

    Changed from SNAPSHOT to version 0.1.
    git-svn-id: http://ala-hubs.googlecode.com/svn/trunk/biocache-hubs@1363 1df052cb-f7c2-36cd-ec53-7bc1070de0c5

--- BiocacheHubsGrailsPlugin.groovy
+++ BiocacheHubsGrailsPlugin.groovy
@@ -18,7 +18,7 @@ import grails.util.Environment
 
 class BiocacheHubsGrailsPlugin {
     // the plugin version
-    def version = "1.0-SNAPSHOT"
+    def version = "0.1"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.3 > *"
     // resources that are excluded from plugin packaging
