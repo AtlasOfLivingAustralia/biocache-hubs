d6f9be8da16a0e555744e5b0b3acc0b4aea4dec2
nickdos <nickdos@gmail.com>
2014-07-21 17:37:47 +1000

    Added toBoolean() to config var value so it will work with a properties files value of false.

--- grails-app/controllers/au/org/ala/biocache/hubs/OccurrenceController.groovy
+++ grails-app/controllers/au/org/ala/biocache/hubs/OccurrenceController.groovy
@@ -47,14 +47,14 @@ class OccurrenceController {
         }
 
         if (!params.sort && !params.dir) {
-            requestParams.sort = "first_loaded_date"
+            requestParams.sort = "first_loaded_date"x
             requestParams.dir = "desc"
         }
 
         List taxaQueries = (ArrayList<String>) params.list("taxa") // will be list for even one instance
         log.debug "skin.useAlaBie = ${grailsApplication.config.skin.useAlaBie}"
 
-        if (grailsApplication.config.skin.useAlaBie && grailsApplication.config.skin.bie.baseUrl && !params.q && taxaQueries && taxaQueries[0]) { // check for list with empty string
+        if (grailsApplication.config.skin.useAlaBie?.toBoolean() && grailsApplication.config.skin.bie.baseUrl && !params.q && taxaQueries && taxaQueries[0]) { // check for list with empty string
             // taxa query - attempt GUID lookup via BIE
             List guidsForTaxa = webServicesService.getGuidsForTaxa(taxaQueries)
             requestParams.q = postProcessingService.createQueryWithTaxaParam(taxaQueries, guidsForTaxa)
