265a5ac9dabb0800ffad84983506f6c943a69238
nickdos <nickdos@gmail.com>
2014-09-01 13:58:47 +1000

    Version 0.55

--- BiocacheHubsGrailsPlugin.groovy
+++ BiocacheHubsGrailsPlugin.groovy
@@ -20,7 +20,7 @@ import grails.util.Environment
 
 class BiocacheHubsGrailsPlugin {
     // the plugin version
-    def version = "0.54"
+    def version = "0.55"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.3 > 2.4"
     // resources that are excluded from plugin packaging
