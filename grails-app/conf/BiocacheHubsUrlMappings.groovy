1568c44955f1d5beedfb1c14913a6bf6a74e513d
nickdos <nickdos@gmail.com>
2016-02-04 15:17:24 +1100

    Fix for deleted url mapping entry

--- grails-app/conf/BiocacheHubsUrlMappings.groovy
+++ grails-app/conf/BiocacheHubsUrlMappings.groovy
@@ -6,6 +6,7 @@ class BiocacheHubsUrlMappings {
         "/occurrences"(redirect: [controller: 'occurrences', action: 'search'])
         "/occurrences/searchByArea"(redirect: [controller: 'occurrences', action: 'search'])
         "/occurrences/search"(controller: 'occurrence', action: 'list')
+        "/occurrence/search"(controller: 'occurrence', action: 'list')
         "/occurrences/search/taxa"(controller: 'occurrence', action: 'taxaPost')
         "/occurrences/taxa/$id"(controller: 'occurrence', action: 'taxa')
         "/occurrences/assertions/add"(controller: 'assertions', action: 'addAssertion')
