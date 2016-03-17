a1f81371a42db409eacdc68372c30de11f2fe128
Peter Ansell <p_ansell@yahoo.com>
2016-03-18 09:44:37 +1100

    Bump to next snapshot development version

--- BiocacheHubsGrailsPlugin.groovy
+++ BiocacheHubsGrailsPlugin.groovy
@@ -20,7 +20,7 @@ import grails.util.Environment
 
 class BiocacheHubsGrailsPlugin {
     // the plugin version
-    def version = "0.75"
+    def version = "0.76-SNAPSHOT"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.3 > *"
     // resources that are excluded from plugin packaging
