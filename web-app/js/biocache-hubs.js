e91f26a330522d6c13ce7426ec780ef58f47d05d
nickdos <nickdos@gmail.com>
2016-11-11 14:36:22 +1100

    Added check for BC_CONF
    To prevent errors in browser console for older hubs.

--- web-app/js/biocache-hubs.js
+++ web-app/js/biocache-hubs.js
@@ -4,9 +4,11 @@
 /************************************************************\
  * i18n
  \************************************************************/
-jQuery.i18n.properties({
-    name: 'messages',
-    path: BC_CONF.contextPath + '/messages/i18n/',
-    mode: 'map',
-    language: BC_CONF.locale // default is to use browser specified locale
-});
+if (typeof BC_CONF != 'undefined' && BC_CONF.hasOwnProperty('contextPath')) {
+    jQuery.i18n.properties({
+        name: 'messages',
+        path: BC_CONF.contextPath + '/messages/i18n/',
+        mode: 'map',
+        language: BC_CONF.locale // default is to use browser specified locale
+    });
+}
