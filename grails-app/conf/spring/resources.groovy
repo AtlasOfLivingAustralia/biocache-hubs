c0903335f8cb4caa65e81eebb274cd6c0d4734d9
nickdos <nickdos@gmail.com>
2017-12-11 17:34:33 +1100

    Custom message source bean

---
+++ grails-app/conf/spring/resources.groovy
@@ -0,0 +1,27 @@
+/*
+ * Copyright (C) 2017 Atlas of Living Australia
+ * All Rights Reserved.
+ * The contents of this file are subject to the Mozilla Public
+ * License Version 1.1 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of
+ * the License at http://www.mozilla.org/MPL/
+ * Software distributed under the License is distributed on an "AS
+ * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * rights and limitations under the License.
+ */
+
+import au.org.ala.biocache.hubs.EnglishValueConverter
+import au.org.ala.biocache.hubs.ExtendedPluginAwareResourceBundleMessageSource
+
+beans = {
+    // Custom message source
+    customMessageSource(ExtendedPluginAwareResourceBundleMessageSource) {
+        // The standard messageSource will already use "WEB-INF/grails-app/i18n/messages"
+        // ExtendedPluginAwareResourceBundleMessageSource uses messageSource as an additional backing message source
+        basename = "${application.config.biocache.baseUrl}/facets/i18n"
+        cacheSeconds = (60 * 60 * 6) // 6 hours
+        useCodeAsDefaultMessage = false
+    }
+
+}
