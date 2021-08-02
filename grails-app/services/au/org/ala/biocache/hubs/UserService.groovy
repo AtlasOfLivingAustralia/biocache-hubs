d86a1c3f998a09826b2256f40b9e79c64f65b52c
alexhuang091 <hua091@csiro.au>
2021-08-02 11:38:06 +1000

    make 'disableAll' and 'expand' configurable

--- grails-app/services/au/org/ala/biocache/hubs/UserService.groovy
+++ grails-app/services/au/org/ala/biocache/hubs/UserService.groovy
@@ -25,6 +25,12 @@ class UserService {
     @Value('${dataquality.enabled}')
     boolean dataQualityEnabled
 
+    @Value('${dataquality.disableAllFilters:false}')
+    boolean disableAllFilters
+
+    @Value('${dataquality.expandFilters:true}')
+    boolean expandFilters
+
     /**
      * Get both email and displayName for a numeric user id.  Preferring to use the auth service
      * unless it's unavailable, then fall back to database
@@ -69,9 +75,9 @@ class UserService {
             }
 
             // make sure values exists
-            pref.disableAll = pref?.containsKey('disableAll') ? pref.disableAll : false
+            pref.disableAll = pref?.containsKey('disableAll') ? pref.disableAll : disableAllFilters
             pref.dataProfile = pref?.containsKey('dataProfile') ? pref.dataProfile : null
-            pref.expand = pref?.containsKey('expand') ? pref.expand : true
+            pref.expand = pref?.containsKey('expand') ? pref.expand : expandFilters
         }
         pref
     }
