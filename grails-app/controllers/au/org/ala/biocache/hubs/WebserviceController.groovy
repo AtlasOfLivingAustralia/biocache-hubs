c1dc72deb6ba44c8ae43ded7696900894ab04608
alexhuang091 <hua091@csiro.au>
2021-08-12 10:36:02 +1000

    comment

--- grails-app/controllers/au/org/ala/biocache/hubs/WebserviceController.groovy
+++ grails-app/controllers/au/org/ala/biocache/hubs/WebserviceController.groovy
@@ -1,10 +1,29 @@
+/*
+ * Copyright (C) 2021 Atlas of Living Australia
+ * All Rights Reserved.
+ * The contents of this file are subject to the Mozilla Public
+ * License Version 1.1 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of
+ * the License at http://www.mozilla.org/MPL/
+ * Software distributed under the License is distributed on an "AS
+ * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * rights and limitations under the License.
+ */
+
 package au.org.ala.biocache.hubs
 
 import grails.converters.JSON
 
+/**
+ * Controller for web services
+ */
 class WebserviceController {
     def webServicesService, authService
 
+    /**
+     * Get alerts current user subscribed to from alert service.
+     */
     def getAlerts() {
         String userId = authService?.getUserId()
         if (userId == null) {
@@ -15,6 +34,9 @@ class WebserviceController {
         }
     }
 
+    /**
+     * Subscribe current user to my annotation alert.
+     */
     def subscribeMyAnnotation() {
         String userId = authService?.getUserId()
         if (userId == null) {
@@ -25,6 +47,9 @@ class WebserviceController {
         }
     }
 
+    /**
+     * Unsubscribe current user from my annotation alert.
+     */
     def unsubscribeMyAnnotation() {
         String userId = authService?.getUserId()
         if (userId == null) {
@@ -35,7 +60,12 @@ class WebserviceController {
         }
     }
 
-    def getStates() {
-        render webServicesService.getStates(params.country) as JSON
+    /**
+     * Get the list of states belong to provided country.
+     *
+     * @param country - name of the country
+     */
+    def getStates(String country) {
+        render webServicesService.getStates(country) as JSON
     }
 }
