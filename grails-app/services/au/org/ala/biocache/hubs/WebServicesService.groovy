AtlasOfLivingAustralia/biocache-hubs

commit c8c6304779f6f5356d3d987904ab850d1c11e379

Issue 334 update ehcache (#488)
updated to ehcache 3.0

grails-app/services/au/org/ala/biocache/hubs/WebServicesService.groovy
@@ -22,6 +22,7 @@ import groovyx.net.http.Method
 import org.apache.commons.httpclient.HttpClient
 import org.apache.commons.httpclient.methods.HeadMethod
 import org.apache.commons.io.FileUtils
+import org.grails.plugin.cache.GrailsCacheManager
 import org.grails.web.json.JSONArray
 import org.grails.web.json.JSONElement
 import org.grails.web.json.JSONObject
@@ -158,9 +159,8 @@ class WebServicesService {
         getJsonElements(url)
     }
 
-    @Cacheable(value="longTermCache", key = "#root.method.name")
+    @Cacheable(value="longTermCache")
     def Map getGroupedFacets() {
-        log.info "Getting grouped facets with key: #root.methodName"
         def url = "${grailsApplication.config.biocache.baseUrl}/search/grouped/facets"
 
         if (grailsApplication.config.biocache.groupedFacetsUrl) {

build.gradle
@@ -44,6 +44,7 @@ dependencies {
     def noCache = {
         exclude group: 'org.grails.plugins', module: 'cache'
         exclude group: 'org.ehcache', module: 'ehcache'
+        exclude group: 'org.grails.plugins', module: 'cache-ehcache'
     }
 
     compile "org.springframework.boot:spring-boot-starter-logging"
@@ -53,7 +54,8 @@ dependencies {
     compile "org.springframework.boot:spring-boot-starter-tomcat"
     compile "org.grails:grails-dependencies"
     compile "org.grails:grails-web-boot"
-    compile "org.grails.plugins:cache"
+    compile "org.grails.plugins:cache:4.0.0"
+    compile "org.grails.plugins:cache-ehcache:3.0.0"
     compile "org.grails.plugins:scaffolding"
     console "org.grails:grails-console"
     profile "org.grails.profiles:web-plugin"

grails-app/conf/ehcache.xml
@@ -1,0 +1,33 @@
+<config xmlns:xsi='http://www.w3.org/2001/XMLSchema-instance'
+        xmlns='http://www.ehcache.org/v3'
+        xmlns:jsr107='http://www.ehcache.org/v3/jsr107'>
+
+    <persistence directory="/data/ala-hub/ehcache"/>
+
+    <cache alias="longTermCache">
+        <expiry>
+            <ttl unit="days">30</ttl>
+        </expiry>
+        <resources>
+            <heap unit="entries">50000</heap>
+        </resources>
+    </cache>
+
+    <cache alias="collectoryCache">
+        <expiry>
+            <ttl unit="hours">24</ttl>
+        </expiry>
+        <resources>
+            <heap unit="entries">1200</heap>
+        </resources>
+    </cache>
+
+    <cache alias="excludedCountCache">
+        <expiry>
+            <ttl unit="hours">2</ttl>
+        </expiry>
+        <resources>
+            <heap unit="entries">100000</heap>
+        </resources>
+    </cache>
+</config>

grails-app/controllers/au/org/ala/biocache/hubs/AdminController.groovy
@@ -41,6 +41,7 @@ class AdminController {
         message += webServicesService.doClearLongTermCache()
         message += doClearFacetsCache()
         message += doClearPropertiesCache()
+        message += doClearRecordCountCache()
         message
     }
 
@@ -64,6 +65,11 @@ class AdminController {
         redirect(action:'index')
     }
 
+    def clearRecordCountCache() {
+        flash.message = doClearRecordCountCache()
+        redirect(action: 'index')
+    }
+
     def doClearFacetsCache() {
         facetsCacheService.clearCache()
         "facetsCache cache cleared\n"
@@ -74,10 +80,7 @@ class AdminController {
         "i18n messages cache cleared\n"
     }
 
-    def clearRecordCountCache() {
+    def doClearRecordCountCache() {
         qualityService.clearRecordCountCache()
-        flash.message = "record count cache cleared\n"
-        redirect(action: 'index')
     }
-
 }

grails-app/services/au/org/ala/biocache/hubs/QualityService.groovy
@@ -20,8 +20,8 @@ import au.org.ala.dataquality.model.QualityCategory
 import au.org.ala.dataquality.model.QualityFilter
 import au.org.ala.dataquality.model.QualityProfile
 import com.google.common.base.Stopwatch
-import com.google.common.cache.Cache
-import com.google.common.cache.CacheBuilder
+import grails.plugin.cache.CacheEvict
+import grails.plugin.cache.Cacheable
 import org.springframework.beans.factory.annotation.Value
 import retrofit2.Call
 import retrofit2.HttpException
@@ -43,15 +43,10 @@ class QualityService {
     @Value('${dataquality.baseUrl}')
     def dataQualityBaseUrl
 
-    @Value('${dataquality.recordCountCacheSpec}')
-    String recordCountCacheSpec
-
     def grailsApplication
 
     QualityServiceRpcApi api
     DataProfilesApi dataProfilesApi
-
-    Cache<SpatialSearchRequestParams, Long> recordCountCache
 
     @PostConstruct
     def init() {
@@ -65,7 +60,6 @@ class QualityService {
             api = apiClient.createService(QualityServiceRpcApi)
             dataProfilesApi = apiClient.createService(DataProfilesApi)
         }
-        recordCountCache = CacheBuilder.from(recordCountCacheSpec).build { webServicesService.fullTextSearch(it)?.totalRecords }
     }
 
     Map<String, String> getEnabledFiltersByLabel(String profileName) {
@@ -156,8 +150,14 @@ class QualityService {
         return profileName && isProfileEnabled(profileName)
     }
 
+    @CacheEvict(value='excludedCountCache', allEntries=true)
     def clearRecordCountCache() {
-        recordCountCache.invalidateAll()
+        "record count cache cleared\n"
+    }
+
+    @Cacheable(value = 'excludedCountCache', key = {requestParams.toString()})
+    def getExcludeCount(SpatialSearchRequestParams requestParams) {
+        return webServicesService.fullTextSearch(requestParams)?.totalRecords
     }
 
     private Long countRecordsExcludedByLabel(List<String> otherLabels, SpatialSearchRequestParams requestParams) {
@@ -172,7 +172,7 @@ class QualityService {
             it.disableQualityFilter = otherLabels
             it
         }
-        recordCountCache.get(srp)
+        grailsApplication.mainContext.getBean('qualityService').getExcludeCount(srp)
     }
 
     Long countTotalRecords(SpatialSearchRequestParams requestParams) {
@@ -189,7 +189,7 @@ class QualityService {
                 it.disableAllQualityFilters = true
                 it
             }
-            return recordCountCache.get(srp)
+            grailsApplication.mainContext.getBean('qualityService').getExcludeCount(srp)
         } else {
             return 0
         }

commit 4dd35d765688a509f2ee6831e74a00054af6e73c

#529 Fix for silent failing of non-200 requests
Revert of change c8c6304 which broke/caused the issue.

grails-app/services/au/org/ala/biocache/hubs/WebServicesService.groovy
@@ -22,6 +22,8 @@ import groovyx.net.http.Method
 import org.apache.commons.httpclient.HttpClient
 import org.apache.commons.httpclient.methods.HeadMethod
 import org.apache.commons.io.FileUtils
+import org.apache.commons.io.IOUtils
+import org.grails.web.converters.exceptions.ConverterException
 import org.grails.web.json.JSONArray
 import org.grails.web.json.JSONElement
 import org.grails.web.json.JSONObject
@@ -432,7 +434,7 @@ class WebServicesService {
      * @return the object we request or an JSON object containing error info in case of error
      */
     JSONElement getJsonElements(String url, String apiKey = null) {
-        log.debug "(internal) getJson URL = " + url
+        log.debug "(internal) getJson URL == " + url
         def conn = new URL(url).openConnection()
         try {
             conn.setConnectTimeout(10000)
@@ -442,16 +444,31 @@ class WebServicesService {
             }
 
             InputStream stream = null;
+            def responseCode = null
+
             if (conn instanceof HttpURLConnection) {
-                conn.getResponseCode() // this line required to trigger parsing of response
-                stream = conn.getErrorStream() ?: conn.getInputStream()
+                log.debug "Open HttpURLConnection"
+                responseCode = conn.getResponseCode() // this line required to trigger parsing of response
+
+                if (responseCode == 200) {
+                    stream = conn.getErrorStream() ?: conn.getInputStream()
+                } else {
+                    throw new IOException("Got a non-200 response from server (${responseCode}) for URL: ${url}", null)
+                }
             } else { // when read local files it's a FileURLConnection which doesn't have getErrorStream
+                log.debug "Open file connection"
                 stream = conn.getInputStream()
             }
-            return JSON.parse(stream, "UTF-8")
+
+            JSONElement jsonOut = JSON.parse(stream, "UTF-8")
+            return jsonOut
+        } catch (ConverterException ce) {
+            def error = "Failed to parse json from web service. ${ce.getClass()} ${ce.getMessage()}"
+            log.error error, ce
+            throw new RestClientException(error, ce)
         } catch (Exception e) {
-            def error = "Failed to get json from web service (${url}). ${e.getClass()} ${e.getMessage()}, ${e}"
-            log.error error
+            def error = "Failed to get json from web service (${url}). ${e.getClass()} ${e.getMessage()}"
+            log.error error, e
             throw new RestClientException(error, e)
         }
     }